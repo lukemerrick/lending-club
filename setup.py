862a5d3176be5dc5177c9230e2d0d5888ed77c65
Luke Merrick <contact@lukemerrick.com>
2018-12-13 15:00:10 -0500

    Initialize repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning on Lending Club p2p marketplace loan data.',
+    author='Luke Merrick',
+    license='',
+)
