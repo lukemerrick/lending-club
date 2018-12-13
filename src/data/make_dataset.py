533506af65b9e7df11dbf37f9c960f389af5b4e7
Luke Merrick <contact@lukemerrick.com>
2018-12-13 15:28:06 -0500

    Adds data download via Kaggle API

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+
+import kaggle
+
+KAGGLE_DATASET = 'wordsforthewise/lending-club'
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(project_path):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    # download the file and unzip using the Kaggle API
+    dataset_path = project_path / 'data' / 'raw'
+    kaggle.api.dataset_download_files(KAGGLE_DATASET, dataset_path,
+                                  quiet=False, unzip=True)
 
 
 if __name__ == '__main__':
@@ -23,8 +27,4 @@ if __name__ == '__main__':
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    main(project_dir)
